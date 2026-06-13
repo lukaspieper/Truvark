ea9050c7a770cb6060feefff934c6c697ce5ef3d
Lukas Pieper <30661176+lukaspieper@users.noreply.github.com>
2026-06-13 21:15:33 +0200

    Update app version to 2.0.2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -132,8 +132,8 @@ android {
     defaultConfig {
         minSdk = 31
         targetSdk = 36
-        versionCode = 201
-        versionName = "2.0.1"
+        versionCode = 202
+        versionName = "2.0.2"
 
         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
 
